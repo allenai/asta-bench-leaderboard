9c1885054b95229d13825ab173890a677f2489f8
Amber Tanaka <96264681+AmberRose2@users.noreply.github.com>
2025-08-06 13:43:12 -0700

    Little arrow button to take you back up (#24)

--- content.py
+++ content.py
@@ -407,6 +407,26 @@ span.wrap[tabindex="0"][role="button"][data-editable="false"] {
     grid-row: 2 !important;
     grid-column: 1 / -1 !important;
 }
+.benchmark-header {
+    display: flex !important;
+    align-items: center !important;
+    gap: 20px !important;
+}
+#scroll-up-button {
+    flex-grow: 0;
+    display: flex;
+    color: #032629;
+    background-color: #faf2e9;
+    align-items: flex-end;
+    height: 57px;
+    padding: 0px;
+    padding-bottom: 2px;
+    min-width: 50px;
+}
+.dark #scroll-up-button {
+    color: #faf2e9;
+    background-color: #032629;
+}
 /*------ Submission Page CSS ------*/
 #custom-form-group {
     border: 1px solid #000 !important; 
@@ -432,5 +452,4 @@ span.wrap[tabindex="0"][role="button"][data-editable="false"] {
 .styler {
     background: none;
 }
-
 """
