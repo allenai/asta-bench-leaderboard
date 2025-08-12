2c742e8624e1494c0b395643d4a88aa218776624
Smita R <smita.rao2@gmail.com>
2025-08-12 11:23:29 -0700

    more eval ordering changes (#43)
    Co-authored-by: Smita <>

--- c_and_e.py
+++ c_and_e.py
@@ -3,7 +3,7 @@ from content import CODE_EXECUTION_DESCRIPTION
 from category_page_builder import build_category_page
 
 # Define the category for this page
-CATEGORY_NAME = "Code Execution"
+CATEGORY_NAME = "Code & Execution"
 
 def build_page():
     build_category_page(CATEGORY_NAME, CODE_EXECUTION_DESCRIPTION)
