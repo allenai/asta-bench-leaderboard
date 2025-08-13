2de81a5845289ea05cf62adba6b60e2bd7d8624e
Jason Dunkelberger <dirkraft@gmail.com>
2025-08-13 15:54:52 -0700

    drop comment that i found more noisy than helpful

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ CONTACT_DATASET = f"allenai/asta-bench-internal-contact-info"
 if IS_INTERNAL:
     # datasets backing the internal leaderboard
     SUBMISSION_DATASET = f"allenai/asta-bench-internal-submissions"
-    RESULTS_DATASET = f"allenai/asta-bench-internal-results" # This is the repo_id for LeaderboardViewer
+    RESULTS_DATASET = f"allenai/asta-bench-internal-results"
     LEADERBOARD_PATH = f"allenai/asta-bench-internal-leaderboard"
 else:
     # datasets backing the public leaderboard
     SUBMISSION_DATASET = f"allenai/asta-bench-submissions"
-    RESULTS_DATASET = f"allenai/asta-bench-results" # This is the repo_id for LeaderboardViewer
+    RESULTS_DATASET = f"allenai/asta-bench-results"
     LEADERBOARD_PATH = f"allenai/asta-bench-leaderboard"
 
 if LOCAL_DEBUG:
