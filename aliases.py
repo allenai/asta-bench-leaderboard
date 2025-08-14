8c43f638b1c185148a881e2066efc4530ce7c329
Chloe Alcestes Anastasiades <chloea@allenai.org>
2025-08-14 10:37:36 -0700

    forgot new file

---
+++ aliases.py
@@ -0,0 +1,12 @@
+OPENNESS_ALIASES = {
+    "Open Source + Open Weights": {"Open source & open weights"},
+    "Open Source": {"Open source & closed weights"},
+    "API Available": {"Closed source & API available"},
+    "Closed": {"Closed source & UI only"}
+}
+
+TOOL_USAGE_ALIASES = {
+    "Standard": {},
+    "Custom with Standard Search": {"Custom interface"},
+    "Fully Custom": {"Fully custom"}
+}
